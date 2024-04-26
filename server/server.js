608a28d2161e87057602571d5f4c77bc7522e4b1
alhadji47 <alphaousmane47@gmail.com>
2024-04-25 22:11:22 -0400

    Added all project files

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/api/expenses', require('./routes/expenses'));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
